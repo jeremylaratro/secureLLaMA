501e5da8f6c98f52e7ca3cf5fb3e8cccc3d19a9c
Jay <62393443+jeremylaratro@users.noreply.github.com>
2024-12-23 21:26:51 -0800

    Add files via upload
    initial uploads

---
+++ monitor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+snort -D -i $(ifconfig | awk '{print $1}' | grep : | tr -d : | tr -d lo) -c /etc/snort/snort.lua --rule-path /etc/snort/rules --daq-dir /usr/local/lib/daq -l /var/log/snort &
+
+dnf install clamav freshclam
+
+freshclam
+
+cat <<EOF > /etc/cron.d/clam.sh
+#!/bin/bash
+# Directory to scan
+SCAN_DIR="/"
+
+# Log file for clamscan
+LOG_FILE="/var/log/clamav/daily-scan.log"
+
+# Run clamscan
+clamscan -r $SCAN_DIR --exclude-dir="^/sys" --exclude-dir="^/proc" --exclude-dir="^/dev" --log=$LOG_FILE
+EOF
+
+sudo chmod +x /usr/local/bin/clam.sh
+# Define the cron job
+CRON_JOB="0 2 * * * /etc/cron.d/clam.sh"
+
+# Check if the cron job already exists
+(crontab -l 2>/dev/null | grep -F "$CRON_JOB") && echo "Cron job already exists." && exit 0
+# Add the new cron job
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+echo "Cron job added successfully."
